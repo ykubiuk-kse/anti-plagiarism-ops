faead003ddd3ee5e22823930e175cc8821493760
ykubiuk-kse <ykubiuk@kse.org.ua>
2024-12-13 12:59:25 +0000

    feat: change perl client to custom python client

---
+++ scripts/moss.py
@@ -0,0 +1,64 @@
+import os
+import mosspy
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser(description="Description of your script")
+
+parser.add_argument("--language", type=str, default="cc")
+parser.add_argument("--submissions_folder", type=str)
+
+args = parser.parse_args()
+
+def find_files(folder_path, suffixes=['.cpp'], exclude_patterns=[]):
+    matching_files = []
+    
+    for suffix in suffixes:
+        clean_suffix = suffix if not suffix.startswith('.') else suffix[1:]
+        pattern = f"*.{clean_suffix}"
+        
+        for path in Path(folder_path).rglob(pattern):
+            path_str = str(path)
+            
+            should_exclude = False
+            for exclude in exclude_patterns:
+                if exclude in path_str:
+                    should_exclude = True
+                    break
+            
+            if not should_exclude:
+                matching_files.append(path_str)
+            
+    return matching_files
+
+def get_file_extensions(language):
+   extensions_map = {
+       'c': ['.c', '.h'],
+       'cc': ['.cpp', '.hpp', '.cc', '.h'],
+       'python': ['.py'],
+       'java': ['.java'],
+       'javascript': ['.js'],
+   }
+   
+   language = language.lower()
+   return extensions_map.get(language, [])
+
+userid = os.getenv("MOSS_USER_ID")
+language = args.language
+submissions_folder = args.submissions_folder
+
+exclude = ['cmake-build-debug/CMakeFiles/']
+files = find_files(submissions_folder, get_file_extensions(language=language), exclude_patterns=exclude)
+
+print("Using language: {}".format(language))
+
+m = mosspy.Moss(userid, language)
+
+for file in files:
+    print(file)
+    m.addFile(file)
+
+url = m.send(lambda file_path, display_name: print("*", end="", flush=True))
+print()
+
+print ("Report Url: " + url)
